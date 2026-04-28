80dd55b1933ac56b03859ebef3dfa2b9408dc52f
Draylend <draylend0821@gmail.com>
2026-04-28 00:43:10 -0700

    Add base files for collector

---
+++ src/collector.py
@@ -0,0 +1,23 @@
+# API Authentication - Verify connection to Bluesky servers
+def authentication():
+    return
+
+# Fetch Posts
+def fetchPost():
+    # Make an API call here to Bluesky to retrieve data based on input parameters
+    return
+
+# Extract URLs from comments/posts
+def extractURL(post):
+    # Search for HTML pages in post content or comments
+    return
+
+# Fetch page titles for HTML
+def getPageTitle(html):
+    return
+
+# Write to JSON (extract data)
+def postToJSON(post):
+    # E.g. extract reposts, hearts, number of comments, user, page title (for HTMLS), user, etc.
+    # Store one post per row; write to the same file until 10MB, then close and open a new file
+    return
---
+++ src/collector.sh
@@ -0,0 +1,1 @@
+# This file allows us to run our program (linux)
